29b2e618d6360e2820c6f466e910e418921164cc
Vikas Dongre <zvikasdongre@gmail.com>
2025-05-30 22:02:05 +0530

    refactor: completely build app from scratch using svelte 5

---
+++ src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<svelte:head>
+    <title>MC Status</title>
+    <meta name="application-name" content="MC Status" />
+    <meta name="description" content="Check Status of minecraft server, instantly!" />
+</svelte:head>
+
+<h1>MC Status</h1>
--- svelte.config.js
+++ svelte.config.js
@@ -1,25 +1,15 @@
-import adapter from '@sveltejs/adapter-auto';
-import preprocess from 'svelte-preprocess';
+import adapter from '@sveltejs/adapter-cloudflare';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  preprocess: [
-    preprocess({
-      replace: [
-        ['import.meta.env.VERCEL_ANALYTICS_ID', JSON.stringify(process.env.VERCEL_ANALYTICS_ID)]
-      ],
-
-      postcss: true
-    })
-  ],
-  kit: {
-    adapter: adapter(),
-
-    // Override http methods in the Todo forms
-    methodOverride: {
-      allowed: ['PATCH', 'DELETE']
+    preprocess: vitePreprocess(),
+    kit: {
+        adapter: adapter(),
+    },
+    compilerOptions: {
+        runes: true,
     }
-  }
-};
+}
 
 export default config;
